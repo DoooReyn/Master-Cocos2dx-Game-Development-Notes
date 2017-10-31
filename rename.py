135730212b2354c5e94748c2afa70540150ccbdf
DoooReyn <jl88744653@gmail.com>
2017-11-01 01:35:28 +0800

    修改遍历方法

--- rename.py
+++ rename.py
@@ -4,10 +4,10 @@ import os
 
 exclude_files = ["README.md"]
 
-for root, dirs, files in os.walk("./"):
-    for file in files:
-        if os.path.splitext(file)[1] == '.md':
-            if file not in exclude_files and "基础卷" not in file:
-                new  = file.replace(" ", "\ ")
-                trim = " [基础卷]" + file.replace(" ", "")
-                os.system("git mv \"" + new + "\"" + trim)
+for item in os.listdir('./'):
+    if os.path.isfile(item) \
+    and os.path.splitext(item)[1] == '.md' \
+    and item not in exclude_files \
+    and "基础卷" not in item:
+        trim = " [基础卷]"+item
+        print("git mv {0} {1}".format(item, trim))
